335e8c021dfa12e7baee9997de8015d0506fa8b7
David Vaquero <pepesan@gmail.com>
2023-04-23 18:45:44 +0200

    metidos métodos para listado, creación, lectura, modificación y borrado de datos

--- src/main/java/com/cursosdedesarrollo/webapp01/servlets/JDBCServlet.java
+++ src/main/java/com/cursosdedesarrollo/webapp01/servlets/JDBCServlet.java
@@ -55,8 +55,121 @@ public class JDBCServlet extends HttpServlet {
 
     public void handleRequest(HttpServletRequest request,
     HttpServletResponse response) throws IOException, ServletException {
+        // System.out.println(request.getParameterMap());
+        System.out.println("Method: "+ request.getMethod());
+        System.out.println("Action: " + request.getParameter("action"));
+        switch (request.getParameter("action")) {
+            case "list":
+                this.list(request, response);
+                break;
+            case "create":
+                this.create(request, response);
+                break;
+            case "read":
+                this.read(request, response);
+                break;
+            case "update":
+                this.update(request, response);
+                break;
+            case "delete":
+                this.delete(request, response);
+                break;
+            default:
+                this.list(request, response);
+                break;
+
+        }
+    }
+
+    private void delete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Recoger el Id del elemento a buscar
+        Integer id = Integer.parseInt(request.getParameter("id"));
+        // buscar en la bbdd el elemento con ese id
+        Language language = this.languageRepository.findById(id);
+        // Eliminar el objeto de la bbdd
+        this.languageRepository.deleteById(id);
+        // Deveolver la respuesta
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(200);
+        ServletOutputStream out = response.getOutputStream();
+        // Imprimir datos en la salida JSON
+        StringBuilder salida = new StringBuilder();
+        salida.append(language.toJson());
+        out.print(salida.toString());
+        // Cerrar la salida
+        out.close();
+    }
+
+    private void update(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Recoger el Id del elemento a buscar
+        Integer id = Integer.parseInt(request.getParameter("id"));
+        // buscar en la bbdd el elemento con ese id
+        Language language = this.languageRepository.findById(id);
+        // Recoger los datos desde la petición
+        String name = request.getParameter("name");
+        String lastUpdate = request.getParameter("lastUpdate");
+        // Modifico el objeto con los datos de la petición
+        language.setName(name);
+        language.setLastUpdate(lastUpdate);
+        // guardar los datos en la bbdd
+        languageRepository.updateById(id, language);
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(200);
+        ServletOutputStream out = response.getOutputStream();
+        // Imprimir datos en la salida JSON
+        StringBuilder salida = new StringBuilder();
+        salida.append(language.toJson());
+        out.print(salida.toString());
+        // Cerrar la salida
+        out.close();
+    }
+
+    private void read(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Recoger el Id del elemento a buscar
+        Integer id = Integer.parseInt(request.getParameter("id"));
+        // buscar en la bbdd el elemento con ese id
+        Language language = this.languageRepository.findById(id);
+        // Deveolver la respuesta
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(200);
+        ServletOutputStream out = response.getOutputStream();
+        // Imprimir datos en la salida JSON
+        StringBuilder salida = new StringBuilder();
+        salida.append(language.toJson());
+        out.print(salida.toString());
+        // Cerrar la salida
+        out.close();
+    }
+
+    private void create(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        // Recoger los datos desde la petición
+        String name = request.getParameter("name");
+        String lastUpdate = request.getParameter("lastUpdate");
+        // limpio los campos de posibles inyecciones
+        // validaciones de los campos
+        // Crear el objeto del leguaje a meter en la BBDD
+        Language language = new Language();
+        language.setName(name);
+        language.setLastUpdate(lastUpdate);
+        System.out.println(language);
+        languageRepository.insert(language);
+        // Deveolver la respuesta
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(200);
+        ServletOutputStream out = response.getOutputStream();
+        // Imprimir datos en la salida JSON
+        StringBuilder salida = new StringBuilder();
+        salida.append(language.toJson());
+        out.print(salida.toString());
+        // Cerrar la salida
+        out.close();
+    }
+
+    public void list(HttpServletRequest request,
+                     HttpServletResponse response) throws IOException, ServletException{
         var listado = this.languageRepository.findAll();
         response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(200);
         ServletOutputStream out = response.getOutputStream();
         // Imprimir datos en la salida JSON
         StringBuilder salida = new StringBuilder();
